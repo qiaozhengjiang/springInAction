d1c3f87eb885848306442ff07be6817fdc6dc8b5
qiaozhengjiang <371911978@qq,com>
2019-01-28 14:26:17 +0800

    create project

---
+++ spingInAction/springInAction/src/main/java/com/prac/springInAction/App.java
@@ -0,0 +1,13 @@
+package com.prac.springInAction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
